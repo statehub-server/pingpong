8959724941e23e91efe7b947672c85f1ae64615f
Olivia <109307370+nshebang@users.noreply.github.com>
2025-08-11 00:13:41 -0600

    feat: add whoami testing rpcs

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,23 @@ import { Router } from 'express'
 
 initModule({
   commands: [
-    { command: 'ping', handlerId: 'ping', broadcast: false, auth: false }
+    { command: 'ping', handlerId: 'ping' },
+    { command: 'whoami', handlerId: 'whoami' }
   ]
 })
 
 onRPCInvoke((msg?: ActionMessage) => {
   if (!msg?.id || !msg.handlerId) return
-  
+
   if (msg.handlerId === 'ping') {
     log('WebSocket ping received')
     reply(msg.id, { message: 'pong' })
+  }
+
+  if (msg.handlerId === 'whoami') {
+    if (!msg.user)
+      return reply(msg.id, { message: `You don't have permission to do that` })
+    reply(msg.id, { message: `Your name is ${msg.user.username || 'unknown'}` })
   }
 })
 
@@ -27,6 +34,18 @@ const router = Router()
 router.get('/ping', (req, res) => {
   log('HTTP ping received')
   res.json({ message: 'pong' })
+})
+
+router.get('/whoami', (req, res) => {
+  const user = (req as any).user
+  if (!user)
+    res.status(403).json({
+      message: `You don't have permission to access this resource`
+    })
+  
+  res.json({
+    message: `Your name is ${user.username || 'unknown'}`
+  })
 })
 
 export { router }
